bba9a7070c90a4094374fda836b63f94cf3c8da8
YusufJake <yusufjohan05@gmail.com>
2020-12-21 20:59:24 +0700

    Server Config Rest sudah siap

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20180140067_Yusuf_Johan_Kelana;
+
+namespace ServerConfigRest_20180140067_Yusuf_Johan_Kelana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Sudah Siap ...");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
